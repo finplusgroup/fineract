3b8ef78bae03dd1b81876d667bc9f9f6751a4fae
rmatovu987 <matovu@finplusgroup.com>
2023-02-03 14:08:22 +0300

    Reinstate system user

--- fineract-provider/src/main/java/org/apache/fineract/useradministration/service/AppUserConstants.java
+++ fineract-provider/src/main/java/org/apache/fineract/useradministration/service/AppUserConstants.java
@@ -29,5 +29,5 @@ public final class AppUserConstants {
     public static final String CLIENTS = "clients";
 
     // TODO: Remove hard coding of system user name and make this a configurable parameter
-    public static final String SYSTEM_USER_NAME = "system";
+    public static final String SYSTEM_USER_NAME = "finplus";
 }
