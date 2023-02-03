cdc0cb3df000e9d3b1d1060ce36c7c1afaea5b67
rmatovu987 <matovu@finplusgroup.com>
2023-02-03 16:03:05 +0300

    Reinstate system user

--- fineract-provider/src/main/java/org/apache/fineract/useradministration/domain/AppUserRepositoryWrapper.java
+++ fineract-provider/src/main/java/org/apache/fineract/useradministration/domain/AppUserRepositoryWrapper.java
@@ -34,9 +34,9 @@ public class AppUserRepositoryWrapper {
     }
 
     public AppUser fetchSystemUser() {
-        AppUser user = this.appUserRepository.findAppUserByName("finplus");
+        AppUser user = this.appUserRepository.findAppUserByName(AppUserConstants.SYSTEM_USER_NAME);
         if (user == null) {
-            throw new UserNotFoundException("finplus");
+            throw new UserNotFoundException(AppUserConstants.SYSTEM_USER_NAME);
         }
         return user;
     }
