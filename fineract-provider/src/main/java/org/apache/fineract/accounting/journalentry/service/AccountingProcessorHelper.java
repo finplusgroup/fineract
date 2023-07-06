fe99d370ad206aac94d1bd182862e11f4860b6c4
matovu <matovu@finplusgroup.com>
2023-07-06 12:35:23 +0300

    Fix importation bug

--- fineract-provider/src/main/java/org/apache/fineract/accounting/journalentry/service/AccountingProcessorHelper.java
+++ fineract-provider/src/main/java/org/apache/fineract/accounting/journalentry/service/AccountingProcessorHelper.java
@@ -23,6 +23,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
